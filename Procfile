091460ca346fe91a7ce1e302c004eaae9165832d
Aakash Goyal <mail@asglabs.in>
2020-10-11 01:15:17 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath indiaware indiaware.wsgi --log-file -
---
+++ project-name-app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IndiawareMapsConfig(AppConfig):
+    name = 'project-name-app'
